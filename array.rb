f3e5c579809c669639397522345ca1d06780fce1
Sonali Sharma <sonalisharma58521@gmail.com>
2018-06-20 15:43:28 +0000

    Done.

---
+++ array.rb
@@ -0,0 +1,26 @@
+# student_one = "Caroline"
+# student_two = "Millie"
+# student_three = "Sam"
+# student_four = "Sonali"
+
+# puts"In this class, we have a student named #{student_four}"
+
+# student_list = ["Caroline", "Millie", " Sonali", "Sam"]
+# puts student_list
+
+# friends_list = ["Joey","Chandler","Phoebe","Ross","Rachel","Ross"]
+# puts friends_list 
+
+disney_characters = ["Simba","Mulan","Stich","The_Little_Mermaid","Meredith"]
+disney_characters.push("PoohBear","Tiger")
+disney_characters
+puts disney_characters.length
+
+age = [17,16,15,13]
+# puts age
+
+favorite_books = ["Everything Everything","Ready Player One","Red Queen","Carry On"]
+# puts favorite_books[3]
+
+favorite_books.push("Lola")
+# puts favorite_books
--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,24 +9,32 @@ STUDENT_NAMES = [
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES[0]
 end
-
+first_student_by_index
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES[3]
 end
-
+fourth_student_by_index
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
+  puts STUDENT_NAMES[5]
 end
-
+last_student_by_index
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  puts STUDENT_NAMES.first
 end
-
+first_student_by_method
 def last_student_by_method
+ puts STUDENT_NAMES.last
   # Write a solution that returns the first student using the built in .last method
 end
-
+last_student_by_method
 def first_second_and_third_students
+ puts STUDENT_NAMES[0,1,]
+ puts STUDENT_NAMES.last
   # Write a solution that returns the first, second and third students
 end
+first_second_and_third_students
